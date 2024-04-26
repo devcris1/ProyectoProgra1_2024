2e49d9fe3b2ccdd410fc157f5afdbaf9607d345f
Cristopher Arana <caranac3@miumg.edu.gt>
2024-04-25 21:42:42 -0600

    Posicionamiento

--- ProyectoProgra1_2024/main.cpp
+++ ProyectoProgra1_2024/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+
+
+    HANDLE consola;
+    consola=GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD posicion;
+    posicion.X = 18;
+    posicion.Y = 15;
+
+    SetConsoleCursorPosition(consola, posicion);
+    cout<<"*";
+
     return 0;
 }
