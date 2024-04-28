158be80059d8c55f74abde6870182a00e8a0da7c
Cristopher Arana <caranac3@miumg.edu.gt>
2024-04-27 23:33:05 -0600

    Variables

--- ProyectoProgra1_2024/main.cpp
+++ ProyectoProgra1_2024/main.cpp
@@ -2,24 +2,52 @@
 #include <conio.h>
 #include <stdio.h>
 #include <windows.h>
-
-
+#include <stdlib.h>
 
 using namespace std;
 
+
 int main()
 {
+    gotoxy(15,60);
+    int x=60, y=15;
+    int i, j, filas;
 
+    bool salir = false;
+    while (!salir){
+    gotoxy(x,y);
+    if (kbhit()){
+        char tecla = getch();
+        if(tecla==IZQUIERDA)x=x-1;
+        if(tecla==DERECHA)x=x+1;
+        if(tecla==ARRIBA)y=y-1;
+        if(tecla==ABAJO)y=y+1;
+    }
+    }
 
+    int n;
+    cout << "Introduce la altura del triangulo: ";
+    cin >> n;
 
-    HANDLE consola;
-    consola=GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD posicion;
-    posicion.X = 18;
-    posicion.Y = 15;
+    // Recorre la altura
+    for (i = 1; i <= n - 1; i++)
+    {
+        // Imprime espacios en blanco por la izquierda
+        for (j = 1; j <= n - i; j++)
+            cout << " ";
 
-    SetConsoleCursorPosition(consola, posicion);
-    cout<<"*";
+        // Imprime los caracteres "*" y " "
+        for (j = 1; j <= 2 * i - 1; j++)
+            if (j == 1 || j == 2 * i - 1)
+                cout << "*";
+            else
+                cout << " ";
+
+            cout << endl;
+    }
+    //Imprime "*" última fila
+    for (i = 1; i <= 2 * n - 1; i++)
+        cout << "*";
 
     return 0;
 }
